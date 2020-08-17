e3064eb64d2bc1fa3c973f6734c3f3f21f1603b9
tastaslim <taslimarif600@gmail.com>
2020-08-17 21:44:42 +0530

    Added Tree

--- Data Structures and Algorithms/Tree/TreeNodeClass.cpp
+++ Data Structures and Algorithms/Tree/TreeNodeClass.cpp
@@ -8,7 +8,42 @@
 
 // Okay, what can we do then? If we are able to solve size problem of array
 // We can use it to implement tree.
-//Here comes vector into picture.
+//Here comes vector into picture as vector handles size problems
+#include "CommonTemplate.h"
+template <typename T>
 
-#include <bits/stdc++.h>
-using namespace std;
+// DFS Approach to print ---> There are 3 types:
+// Inorder, Preorder, Postorder
+void print(TreeNode<T> *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << ":";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << ",";
+    }
+    cout << endl;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        print(root->children.at(i));
+    }
+}
+int main()
+{
+    TreeNode<int> *root = new TreeNode<int>(1);
+    TreeNode<int> *root1 = new TreeNode<int>(2);
+    TreeNode<int> *root2 = new TreeNode<int>(3);
+    TreeNode<int> *root3 = new TreeNode<int>(4);
+    TreeNode<int> *root4 = new TreeNode<int>(5);
+    TreeNode<int> *root5 = new TreeNode<int>(6);
+    TreeNode<int> *root6 = new TreeNode<int>(7);
+    root->children.push_back(root1);
+    root->children.push_back(root2);
+    root1->children.push_back(root3);
+    root2->children.push_back(root5);
+    root2->children.push_back(root6);
+    root3->children.push_back(root4);
+    print(root);
+    return 0;
+}
